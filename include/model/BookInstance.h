38f3d0bc95d647499a6ec2159f25d6b101797f40
yummyJade <yuyuan7@126.com>
2019-09-29 10:05:11 +0800

    更改了一下book类，增加了图书搜索关键词功能

---
+++ include/model/BookInstance.h
@@ -0,0 +1,19 @@
+//
+// Created by THINKPAD on 2019/9/28.
+//
+
+#ifndef MYC1_BOOKINSTANCE_H
+#define MYC1_BOOKINSTANCE_H
+
+#endif //MYC1_BOOKINSTANCE_H
+
+class BookInstance: {
+private:
+    int bookId;     //图书的id
+    int status;     //状态
+    std::string position;       // 图书位置
+
+    //    bool isLend = true;        //是否可借
+//    bool isValid = true;       // 是否有效, 有被删除时置为false
+
+};
