e1de2b186b36f6bc308914da3692ec5fab1bce12
MrTjming <mrtjming@gmail.com>
2019-09-29 10:25:36 +0800

    fix sth.

--- include/model/BookInstance.h
+++ include/model/BookInstance.h
@@ -5,15 +5,16 @@
 #ifndef MYC1_BOOKINSTANCE_H
 #define MYC1_BOOKINSTANCE_H
 
-#endif //MYC1_BOOKINSTANCE_H
-
-class BookInstance: {
+class BookInstance : {
 private:
     int bookId;     //图书的id
     int status;     //状态
     std::string position;       // 图书位置
-
+    Date::Date planReturnDate; // 预计归还日期
     //    bool isLend = true;        //是否可借
 //    bool isValid = true;       // 是否有效, 有被删除时置为false
 
 };
+
+#endif //MYC1_BOOKINSTANCE_H
+
