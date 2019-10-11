e0198e3dcfa6c2ff319538b539e274bb0e7548fa
yummyJade <yuyuan7@126.com>
2019-10-11 22:14:56 +0800

    cmake-debug merge前commit了一下

--- cmake-build-debug/CMakeFiles/3.14.5/CMakeCCompiler.cmake
+++ cmake-build-debug/CMakeFiles/3.14.5/CMakeCCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_C_COMPILER "C:/Program Files (x86)/CodeBlocks/MinGW/bin/gcc.exe")
+set(CMAKE_C_COMPILER "D:/Install/CodeBlocks/MinGW/bin/gcc.exe")
 set(CMAKE_C_COMPILER_ARG1 "")
 set(CMAKE_C_COMPILER_ID "GNU")
 set(CMAKE_C_COMPILER_VERSION "5.1.0")
@@ -16,11 +16,11 @@ set(CMAKE_C_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "C:/Program Files (x86)/CodeBlocks/MinGW/bin/ar.exe")
-set(CMAKE_C_COMPILER_AR "C:/Program Files (x86)/CodeBlocks/MinGW/bin/gcc-ar.exe")
-set(CMAKE_RANLIB "C:/Program Files (x86)/CodeBlocks/MinGW/bin/ranlib.exe")
-set(CMAKE_C_COMPILER_RANLIB "C:/Program Files (x86)/CodeBlocks/MinGW/bin/gcc-ranlib.exe")
-set(CMAKE_LINKER "C:/Program Files (x86)/CodeBlocks/MinGW/bin/ld.exe")
+set(CMAKE_AR "D:/Install/CodeBlocks/MinGW/bin/ar.exe")
+set(CMAKE_C_COMPILER_AR "D:/Install/CodeBlocks/MinGW/bin/gcc-ar.exe")
+set(CMAKE_RANLIB "D:/Install/CodeBlocks/MinGW/bin/ranlib.exe")
+set(CMAKE_C_COMPILER_RANLIB "D:/Install/CodeBlocks/MinGW/bin/gcc-ranlib.exe")
+set(CMAKE_LINKER "D:/Install/CodeBlocks/MinGW/bin/ld.exe")
 set(CMAKE_MT "")
 set(CMAKE_COMPILER_IS_GNUCC 1)
 set(CMAKE_C_COMPILER_LOADED 1)
@@ -69,7 +69,7 @@ endif()
 
 
 
-set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "C:/Program Files (x86)/CodeBlocks/MinGW/lib/gcc/mingw32/5.1.0/include;C:/Program Files (x86)/CodeBlocks/MinGW/include;C:/Program Files (x86)/CodeBlocks/MinGW/lib/gcc/mingw32/5.1.0/include-fixed")
+set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "D:/Install/CodeBlocks/MinGW/lib/gcc/mingw32/5.1.0/include;D:/Install/CodeBlocks/MinGW/include;D:/Install/CodeBlocks/MinGW/lib/gcc/mingw32/5.1.0/include-fixed")
 set(CMAKE_C_IMPLICIT_LINK_LIBRARIES "mingw32;gcc;moldname;mingwex;pthread;advapi32;shell32;user32;kernel32;mingw32;gcc;moldname;mingwex")
-set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "C:/Program Files (x86)/CodeBlocks/MinGW/lib/gcc/mingw32/5.1.0;C:/Program Files (x86)/CodeBlocks/MinGW/lib/gcc;C:/Program Files (x86)/CodeBlocks/MinGW/mingw32/lib;C:/Program Files (x86)/CodeBlocks/MinGW/lib")
+set(CMAKE_C_IMPLICIT_LINK_DIRECTORIES "D:/Install/CodeBlocks/MinGW/lib/gcc/mingw32/5.1.0;D:/Install/CodeBlocks/MinGW/lib/gcc;D:/Install/CodeBlocks/MinGW/mingw32/lib;D:/Install/CodeBlocks/MinGW/lib")
 set(CMAKE_C_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
